20b561307d662a45c9ee92f966d86fcf46655ecf
dat <datthangvan@gmail.com>
2020-02-14 23:02:53 +0700

    update

--- GeneticAlgorithms.py
+++ GeneticAlgorithms.py
@@ -2,7 +2,9 @@ from __future__ import print_function
 from __future__ import division
 from __future__ import absolute_import
 
-import numpy
+import numpy as np
+from utils.sort_utils import sort_ascent, sort_descent
+
 
 class GA():
     def __init__(self, init_pop, cost_func, go_min=True):
@@ -18,6 +20,10 @@ class GA():
         self._go_min = go_min
         self._num_select = len(self._init_pop) / 2
 
+        self._sort_func = sort_ascent()
+        if self._go_min == True:
+            self._sort_func = sort_descent()
+
     def __str__(self):
         """
 
@@ -30,6 +36,21 @@ class GA():
 
         :return:
         """
-        fitness = [self._cost_func(invidual) for invidual in self._init_pop]
+        # compute cost function
+        fitness = np.asarray([self._cost_func(invidual) for invidual in self._init_pop])
 
-        
+        # sort
+        fitness, population = self._sort_func(fitness, self._init_pop)
+
+        # remove 50%
+        fitness = fitness[:self._num_select]
+        population = population[:self._num_select]
+
+    def _cross_over(self, parents):
+        """
+
+        :param parents:
+        :return:
+        """
+
+
